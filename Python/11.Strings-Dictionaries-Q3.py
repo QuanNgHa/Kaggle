2c02f1be3c238c5a42187eb1da9a3c5548509591
QuanNgHa <47073386+QuanNgHa@users.noreply.github.com>
2019-06-05 11:05:47 +0800

    Create 11.Strings-Dictionaries-Q3.py

---
+++ Python/11.Strings-Dictionaries-Q3.py
@@ -0,0 +1,15 @@
+def multi_word_search(doc_list, keywords):
+    """
+    Takes list of documents (each document is a string) and a list of keywords.  
+    Returns a dictionary where each key is a keyword, and the value is a list of indices
+    (from doc_list) of the documents containing that keyword
+    >>> doc_list = ["The Learn Python Challenge Casino.", "They bought a car and a casino", "Casinoville"]
+    >>> keywords = ['casino', 'they']
+    >>> multi_word_search(doc_list, keywords)
+    {'casino': [0, 1], 'they': [1]}
+    """
+    dict = {}
+    for keyword in keywords:
+        dict[keyword] = [doc_list.index(doc) for doc in doc_list if keyword.lower()in [token.rstrip('.,').lower() for token in doc.split()]]
+    return dict
+© 2019 GitHub, Inc.
